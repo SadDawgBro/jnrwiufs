f349beb254996f6c73027e72a3b20c61beac9be3
SadDawgBro <123268265+SadDawgBro@users.noreply.github.com>
2023-11-01 23:37:58 +0000

    Create eaugdea.js

---
+++ eaugdea.js
@@ -0,0 +1,5 @@
+var messageHTML = '<div style="width: 705px;height: 90px;background-color: #313131;" id="template"><div style="font-size: 15px;">USERNAME</div><div style="width: 50px;height: 50px;background-color: aqua;"><div style="position: absolute; left: 80px; font-size: 14px;">please stick to the limit</div></div></div>';
+var msg = document.createElement('div');
+msg.style = 'width: 705px;height: 90px;background-color: #313131;';
+msg.innerHTML = messageHTML;
+document.getElementById('messages').appendChild(msg)
